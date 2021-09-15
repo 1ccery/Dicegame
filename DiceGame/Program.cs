909f51f648c75f08d39ea3b0115ce197429d45cd
Opilane TTHK <opilane@tthk.ee>
2021-09-15 09:43:50 +0300

    RandomNumberGeneratos

---
+++ DiceGame/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+
+            string PlayerOne = "Adrian";
+            string PlayerTwo = "Joosep";
+
+
+
+            int JoueThrow = rnd.Next(1, 7);
+            int PapsThrow = rnd.Next(1, 7);
+
+            Console.WriteLine($"{PlayerOne} threw {JoueThrow}");
+            Console.WriteLine($"{PlayerTwo} threw {PapsThrow}");
+
+
+
+
+            if (JoueThrow > PapsThrow)
+            {
+                Console.WriteLine($"{PlayerOne} wins");
+            }
+            else if (JoueThrow < PapsThrow)
+            {
+                Console.WriteLine($"{PlayerTwo} wins");
+
+            }
+            else
+            {
+                Console.WriteLine($"Draw try again");
+
+
+            }
+
+
+
+        }
+    }
+}
+
+
+
+
---
+++ RandomNumberGenerators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int myRandomNumber = rnd.Next(1,11);
+            Console.WriteLine($"Sinu juhuslik number on {myRandomNumber}");
+            //programm kontrollib kas genereeritud nuber on suurem kui 5
+            //kui genereeritud mumber on suurem, kui 5 iis programm kuvab
+            //juhuslik number on {myRandomNumber} see on suurem kui 5
+
+            
+
+            if (myRandomNumber > 5)
+            {
+                Console.WriteLine($"sinu kuvatud {myRandomNumber} on suurem kui 5");
+            }
+            else if (myRandomNumber < 5)
+            {
+                Console.WriteLine($"su {myRandomNumber} on väiksem kui viis");
+
+            }
+            else if (myRandomNumber == 5)
+            {
+                Console.WriteLine($"su {myRandomNumber} on 5");
+
+            }
+        }
+    }
+}
